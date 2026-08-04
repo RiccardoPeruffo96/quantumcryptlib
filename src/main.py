2aef3ccd4eeac90e11488f740f0c9968816e1b5d
RiccardoPeruffo96 <riccardoperuffo96@gmail.com>
2026-08-05 00:46:45 +0200

    Add Weyl Heisenberg Operators Bipartite generation

--- src/main.py
+++ src/main.py
@@ -9,8 +9,11 @@ import sdp.primal
 import sdp.matrix
 
 def main():
-    parser = argparse.ArgumentParser(description="Esegue il circuito CNOT con ancilla usando un qubit di controllo generico.")
-    parser.add_argument("--d", type=int, default=4, help="Qudit dimensionality")
+    parser = argparse.ArgumentParser(description="Program for developing Kanitschar Huber framework for quantum cryptography")
+    parser.add_argument("--d", type=int, default=4, help="Qudit dimension (d >= 2)")
+
+    if parser.parse_args().d < 2:
+        parser.error("Qudit dimension d must be at least 2.")
 
     # d equals qudit dimensionality
     d = parser.parse_args().d
@@ -21,8 +24,16 @@ def main():
     # Phase matrix (d x d)
     Zb_d = sdp.matrix.genPhaseMatrix(d)
 
+    # Generate Weyl Heisenberg local Operators
+    U_local = sdp.matrix.genWeylHeisenbergOperators(d, Xa_d, Zb_d)
+
+    # Generate bipartite operators
+    U_bipartite = sdp.matrix.genBipartiteWeylHeisenbergOperators(d, U_local, U_local)
+
     print("Xa_d (zeros):\n", Xa_d)
     print("\nZb_d (identity):\n", Zb_d)
+    print("\nU_local (Weyl Heisenberg Operators):\n", U_local)
+    print("\nU_bipartite (Bipartite Weyl Heisenberg Operators):\n", U_bipartite)
 
 if __name__ == "__main__":
     main()
--- src/sdp/matrix.py
+++ src/sdp/matrix.py
@@ -1,5 +1,9 @@
 # src/sdp/matrix.py
 
+"""
+This module contains functions to generate matrices 
+"""
+
 import numpy as np
 import numpy.typing as npt
 
@@ -67,4 +71,30 @@ def genWeylHeisenbergOperators(d: int,
         for b in range(d):
             Zb = np.linalg.matrix_power(Zb_d, b)
             U[(a, b)] = Xa @ Zb
-    return U 
+    return U
+
+def genBipartiteWeylHeisenbergOperators(d: int,
+                               U1_d: dict[tuple[int, int], npt.NDArray[np.complex128]] | None = None,
+                               U2_d: dict[tuple[int, int], npt.NDArray[np.complex128]] | None = None
+                               ) -> dict[tuple[int, int, int, int], npt.NDArray[np.complex128]]:
+    """
+    Combine both Weyl Heisenberg operators using Kronecker product to create the d^4 bipartite operators
+    """
+    U_bipartite = {}
+    for (a1, b1), U1 in U1_d.items():
+        for (a2, b2), U2 in U2_d.items():
+            U_bipartite[(a1, b1, a2, b2)] = np.kron(U1, U2)
+    return U_bipartite
+
+def genBipartiteWeylHeisenbergOperators(d: int,
+                               Xa1_d: npt.NDArray[np.complex128] | None = None,
+                               Zb1_d: npt.NDArray[np.complex128] | None = None,
+                               Xa2_d: npt.NDArray[np.complex128] | None = None,
+                               Zb2_d: npt.NDArray[np.complex128] | None = None
+                               ) -> dict[tuple[int, int, int, int], npt.NDArray[np.complex128]]:
+    """
+    Override the previous function to generate bipartite Weyl Heisenberg operators directly from the shift and phase matrices.
+    """
+    U1_d = genWeylHeisenbergOperators(d, Xa1_d, Zb1_d)
+    U2_d = genWeylHeisenbergOperators(d, Xa2_d, Zb2_d)
+    return genBipartiteWeylHeisenbergOperators(d, U1_d, U2_d)
